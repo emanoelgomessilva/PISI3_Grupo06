9b019a1bad94e936587d26c58f957d828377d60b
lucas-daniel202 <lucas.dan.melo@gmail.com>
2023-10-13 23:21:23 -0300

    cabeçalho inicial

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+# Ajuste da página geral
+st.set_page_config(
+    page_title = 'Projeto',
+    page_icon = ':books:',
+    layout = 'wide',
+    menu_items= {
+        'Get help': "https://streamlit.io",
+        'Report a Bug': "https://blog.streamlit.io",
+        'About': "Este é um projeto estudantil feito por alunos da **UFRPE**."
+        } # type: ignore
+)
+
+# Criação de um cabeçalho
+st.markdown('''# **Projeto científico**
+
+Por `arthur`, `emanoel` e `lucas`
+---
+''', unsafe_allow_html=True)
