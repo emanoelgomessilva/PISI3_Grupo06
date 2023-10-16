bc0885d791c872d001fb554d25a9e4c495a15f23
lucas-daniel202 <lucas.dan.melo@gmail.com>
2023-10-16 20:34:09 -0300

    atualização e criação do requirements

--- main.py
+++ main.py
@@ -8,13 +8,27 @@ st.set_page_config(
     menu_items= {
         'Get help': "https://streamlit.io",
         'Report a Bug': "https://blog.streamlit.io",
-        'About': "Este é um projeto estudantil feito por alunos da **UFRPE**."
+        'About': '''Este é um projeto estudantil feito por alunos da **UFRPE**.
+        Acesse: bsi.ufrpe.br
+        '''
         } # type: ignore
 )
 
 # Criação de um cabeçalho
-st.markdown('''# **Projeto científico**
+st.markdown('''
+# **Projeto Científico: Análise de Doenças Cardiovasculares**
 
-Por `arthur`, `emanoel`, `lipe` e `lucas`
+Este é um trabalho científico voltado à análise de dados, usando um <i>dataset</i> de doenças cardiovasculares.
+
+O projeto se concentra no uso de algoritmos de clusterização e classificação para entender padrões e prever a presença de doenças cardiovasculares em pacientes.            
+
+**Membros do Projeto:**
+- `Arthur` (arthur.bbsantos@ufrpe.br)
+- `Emanoel` (emanoel20092009@gmail.com)
+- `Fellipe` (pipo200115@gmail.com)
+- `Lucas` (lucas.dan.melo@gmail.com)
+            
+**Fonte**:
+- [Cardiovascular Disease Dataset](https://www.kaggle.com/datasets/colewelkins/cardiovascular-disease?select=cardio_data_processed.csv)
 ---
 ''', unsafe_allow_html=True)
