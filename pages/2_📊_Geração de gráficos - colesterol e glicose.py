58916ea967adf290b61734b079986d2b98d77696
lucas-daniel202 <lucas.dan.melo@gmail.com>
2023-10-25 16:29:38 -0300

    alterações nos gráficos

--- pages/2_📊_Geração de gráficos - colesterol e glicose.py
+++ pages/2_📊_Geração de gráficos - colesterol e glicose.py
@@ -1,19 +1,36 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+import plotly.graph_objects as go
 
 df = pd.read_csv('data/cardio_data_processed.csv')
 
 def grafico_barra():
-    st.write('**Gráfico de Barra**')
-    stacked_data = df.groupby(["cholesterol", "gluc"]).size().reset_index(name="count")
-    fig_stacked = px.bar(stacked_data, x = "cholesterol", y = "count", color = "gluc", barmode = "stack", title = "Níveis de Colesterol e Glicose")
-    st.plotly_chart(fig_stacked)
+    cholesterol_labels = {1:'Normal', 2:'Acima do Normal', 3:'Muito Acima do Normal'}
+    gluc_labels = {1:'Normal', 2:'Acima do Normal', 3:'Muito Acima do Normal'}
+    
+    fig_cholesterol = go.Figure()
+    fig_gluc = go.Figure()
+
+    stacked_df_cholesterol = df[df['cholesterol'] == df['cholesterol'].unique()[0]].groupby('gluc').size().reset_index(name = 'count')
+    for idx, row in stacked_df_cholesterol.iterrows():
+        fig_cholesterol.add_trace(go.Bar(x = [cholesterol_labels[row['gluc']]], y = [row['count']], name = f"Colesterol {cholesterol_labels[row['gluc']]}", text = [row['count']], textposition = 'inside'))
+
+    stacked_df_gluc = df[df['gluc'] == df['gluc'].unique()[0]].groupby('cholesterol').size().reset_index(name = 'count')
+    for idx, row in stacked_df_gluc.iterrows():
+        fig_gluc.add_trace(go.Bar(x = [gluc_labels[row['cholesterol']]], y = [row['count']], name = f"Glicose {gluc_labels[row['cholesterol']]}", text = [row['count']], textposition = 'inside'))
+
+    fig_cholesterol.update_layout(title = 'Níveis de Colesterol')
+    fig_gluc.update_layout(title = 'Níveis de Glicose')
+
+    st.plotly_chart(fig_cholesterol)
+    st.plotly_chart(fig_gluc)
 
 def grafico_pizza():
     st.write('**Gráfico de Pizza**')
     coluna = st.selectbox('Selecione a coluna', ['Colesterol', 'Glicose'])
-    st.write(f'**Distribuição de {coluna}**')
+    st.write(f'**Porcentagens de {coluna}**')
+    
     if coluna == 'Colesterol':
         fig_pie = px.pie(df, names = 'cholesterol')
         st.plotly_chart(fig_pie)
@@ -21,20 +38,11 @@ def grafico_pizza():
         fig_pie = px.pie(df, names = 'gluc')
         st.plotly_chart(fig_pie)
 
-def grafico_dispersao():
-    st.write('**Gráfico de Dispersão**')
-    fig_scatter = px.scatter(df, x = 'ap_hi', y = 'ap_lo', title = 'Pressão Sanguínea Sistólica (ap_hi) e Diastólica (ap_lo)')
-    st.plotly_chart(fig_scatter)
-
-
-escolha = st.selectbox('**Selecione um gráfico para vizualizar**', ['Barra', 'Pizza', 'Dispersão'])
+escolha = st.selectbox('**Selecione um gráfico para vizualizar**', ['Barra', 'Pizza'])
 
 if escolha == 'Barra':
     st.write('')
     grafico_barra()
-elif escolha == 'Pizza':
-    st.write('')
-    grafico_pizza()
 else:
     st.write('')
-    grafico_dispersao()
+    grafico_pizza()
---
+++ pages/4_📊_Geração de gráficos - Pressão sanguínea.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+df = pd.read_csv('data/cardio_data_processed.csv')
+
+def grafico_dispersao():
+    st.write('**Gráfico de Dispersão**')
+
+    fig_scatter = px.scatter(df, x = 'ap_hi', y = 'ap_lo', title = 'Pressão Sanguínea Sistólica (ap_hi) e Diastólica (ap_lo)')
+    st.plotly_chart(fig_scatter)
+
+def grafico_barra():
+    st.write('**Gráfico de Barras Empilhadas**')
+
+    category_counts = df['bp_category'].value_counts().reset_index()
+    category_counts.columns = ['bp_category', 'count']
+    category_counts = category_counts.sort_values(by = 'count', ascending = False)
+
+    fig_stacked = px.bar(category_counts, x = 'bp_category', y = 'count', title = "Distribuição de Pressão Sanguínea nas Categorias", color = 'bp_category', labels = {"count": "Contagem"}, category_orders = {"bp_category": category_counts['bp_category'].tolist()})
+    st.plotly_chart(fig_stacked)
+
+def grafico_setor():
+    st.write('**Gráfico de Setores**')
+
+    fig_donut = px.pie(df, names = 'bp_category', title = 'Porcentagem das Categorias de Pressão Sanguínea', hole = 0.3)
+    colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd']
+
+    fig_donut.update_traces(marker = dict(colors = colors))
+    st.plotly_chart(fig_donut)
+
+escolha = st.selectbox('**Selecione um gráfico para vizualizar**', ['Dispersão', 'Setores', 'Barra'])
+
+if escolha == 'Dispersão':
+    st.write('')
+    grafico_dispersao()
+elif escolha == 'Setor':
+    st.write('')
+    grafico_setor()
+else:
+    st.write('')
+    grafico_barra()
