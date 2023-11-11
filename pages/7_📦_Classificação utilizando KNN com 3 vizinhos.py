d0969cadf245b9f8b5e606fcc21fd589b88ce177
emanoelgomessilva <emanoel20092009@gmail.com>
2023-11-11 14:31:21 -0300

    atualização do arquivo requirements + adição do gráfico de precisão para 10 amostras no knn + melhorias no código

--- pages/7_📦_Classificação utilizando KNN com 3 vizinhos.py
+++ pages/7_📦_Classificação utilizando KNN com 3 vizinhos.py
@@ -6,106 +6,33 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 df = pd.read_parquet('data/cvd_cleaned.parquet')
 
-df_knn = df[['General_Health','Checkup','Exercise','Heart_Disease','Skin_Cancer','Other_Cancer','Depression','Diabetes','Arthritis','Sex','Age_Category','Height_(cm)','Weight_(kg)','BMI','Smoking_History','Alcohol_Consumption','Fruit_Consumption','Green_Vegetables_Consumption','FriedPotato_Consumption']]
+df_knn = df[['General_Health','Checkup','Exercise','Heart_Disease','Skin_Cancer','Other_Cancer','Depression','Diabetes','Arthritis','Sex','Age_Category','BMI','Smoking_History','Alcohol_Consumption','Fruit_Consumption','Green_Vegetables_Consumption','FriedPotato_Consumption']]
 
-mapeamento_general_health = {
-    'Poor': 0,
-    'Fair': 1,
-    'Good': 2,
-    'Very Good': 3,
-    'Excellent': 4
-}
+df_knn['General_Health'] = LabelEncoder().fit_transform(df_knn['General_Health'])
 
-df_knn['General_Health'] = df['General_Health'].replace(mapeamento_general_health)
+df_knn['Checkup'] = LabelEncoder().fit_transform(df_knn['Checkup'])
 
-mapeamento_checkup = {
-    'Within the past year': 0,
-    'Within the past 2 years': 1,
-    'Within the past 5 years': 2,
-    '5 or more years ago': 3,
-    'Never': 4   
-}
+df_knn['Exercise'] = LabelEncoder().fit_transform(df_knn['Exercise'])
 
-df_knn['Checkup'] = df['Checkup'].replace(mapeamento_checkup)
+df_knn['Skin_Cancer'] = LabelEncoder().fit_transform(df_knn['Skin_Cancer'])
 
-mapeamento_exercise = {
-    'Yes': 1,
-    'No': 0  
-}
+df_knn['Other_Cancer'] = LabelEncoder().fit_transform(df_knn['Other_Cancer'])
 
-df_knn['Exercise'] = df['Exercise'].replace(mapeamento_exercise)
+df_knn['Depression'] = LabelEncoder().fit_transform(df_knn['Depression'])
 
-mapeamento_skin_cancer = {
-    'Yes': 1,
-    'No': 0  
-}
+df_knn['Diabetes'] = LabelEncoder().fit_transform(df_knn['Diabetes'])
 
-df_knn['Skin_Cancer'] = df['Skin_Cancer'].replace(mapeamento_skin_cancer)
+df_knn['Arthritis'] = LabelEncoder().fit_transform(df_knn['Arthritis'])
 
-mapeamento_other_cancer = {
-    'Yes': 1,
-    'No': 0  
-}
+df_knn['Sex'] = LabelEncoder().fit_transform(df_knn['Sex'])
 
-df_knn['Other_Cancer'] = df['Other_Cancer'].replace(mapeamento_other_cancer)
+df_knn['Age_Category'] = LabelEncoder().fit_transform(df_knn['Age_Category'])
 
-mapeamento_depression = {
-    'Yes': 1,
-    'No': 0  
-}
-
-df_knn['Depression'] = df['Depression'].replace(mapeamento_depression)
-
-mapeamento_diabetes = {
-    'Yes': 1,
-    'No': 2,
-    'No, pre-diabetes or borderline diabetes': 3,
-    'Yes, but female told only during pregnancy': 4  
-}
-
-df_knn['Diabetes'] = df['Diabetes'].replace(mapeamento_diabetes)
-
-mapeamento_arthritis = {
-    'Yes': 1,
-    'No': 0  
-}
-
-df_knn['Arthritis'] = df['Arthritis'].replace(mapeamento_arthritis)
-
-mapeamento_sex = {
-    'Male': 1,
-    'Female': 0  
-}
-
-df_knn['Sex'] = df['Sex'].replace(mapeamento_sex)
-
-mapeamento_age_category = {
-    '18-24': 0,
-    '25-29': 1,
-    '30-34': 2,
-    '35-39': 3,
-    '40-44': 4,
-    '45-49': 5,
-    '50-54': 6,
-    '55-59': 7,
-    '60-64': 8,
-    '65-69': 9,
-    '70-74': 10,
-    '75-79': 11,
-    '80+': 12,  
-}
-
-df_knn['Age_Category'] = df['Age_Category'].replace(mapeamento_age_category)
-
-mapeamento_smoking_history = {
-    'Yes': 1,
-    'No': 0  
-}
-
-df_knn['Smoking_History'] = df['Smoking_History'].replace(mapeamento_smoking_history)
+df_knn['Smoking_History'] = LabelEncoder().fit_transform(df_knn['Smoking_History'])
 
 df_knn['Heart_Disease'] = LabelEncoder().fit_transform(df_knn['Heart_Disease'])
 
@@ -121,7 +48,7 @@ k = 3
 knn = KNeighborsClassifier(n_neighbors=k)
 knn.fit(X_train, y_train)
 
-prediction = knn.predict(X)
+prediction = knn.predict(X_test)
 
 color_scale = px.colors.sequential.Viridis
 
@@ -135,6 +62,20 @@ fig = px.scatter(df_knn, x = x_column, y = y_column, color = df_knn['Heart_Disea
 st.plotly_chart(fig)
 st.write('----')
 
-plt.scatter(x_column, y_column, c=prediction[0])
-plt.text(x=x_column-1.7, y=y_column-0.7, s=f"new point, class: {prediction[0]}")
-plt.show()
+st.write('**Precisão do experimento**')
+
+accuracies = []
+
+for size in range(1, 10):
+    X_sample, _, y_sample, _ = train_test_split(X_test, y_test, test_size=(size/10), random_state=42)
+    
+    y_sample_pred = knn.predict(X_sample)
+    
+    accuracy = accuracy_score(y_sample, y_sample_pred)
+    accuracies.append(accuracy)
+
+fig = px.line(x=range(10, 100, 10), y = accuracies)
+fig.update_xaxes(title = 'Tamanho da Amostra (%)')
+fig.update_yaxes(title = 'Precisão do Modelo')
+
+st.plotly_chart(fig)
