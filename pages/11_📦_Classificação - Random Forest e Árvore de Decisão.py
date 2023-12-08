7ec698bdc8c0e47e8f63dd3acd85713777801b2d
lucas-daniel202 <lucas.dan.melo@gmail.com>
2023-12-08 17:20:04 -0300

    ajustes na matriz e feature

--- pages/11_📦_Classificação - Random Forest e Árvore de Decisão.py
+++ pages/11_📦_Classificação - Random Forest e Árvore de Decisão.py
@@ -1,17 +1,20 @@
 import pandas as pd
 import streamlit as st
+from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 
 st.write('<h1>Classificação</h1>', unsafe_allow_html=True)
-st.write('''Para essa classificação, é usado dois algoritmos supervisionados: Random Forest e Árvore de Decisão
-         (<i>Decision Tree</i>). As colunas usadas são colunas referentes a existência ou ausência de outras enfermidades,
-         como câncer de pele, depressão, artrite, diabetes e de mais cânceres, a fim de acertar as ocorrências da coluna
-         que indica presença de doença cardiovascular - chamado também de coluna <i>target</i>. Após essa predição, é 
-         mostrado as porcentagens de acerto das amostras de treino e de teste.''', unsafe_allow_html=True)
+st.write('''Para essa classificação, é usado alguns algoritmos supervisionados: K-Nearest Neighbors (kNN), Suport Vector Machine (SVM),
+         Random Forest e Árvore de Decisão (<i>Decision Tree</i>). As colunas usadas são colunas referentes a 
+         existência ou ausência de outras enfermidades, como câncer de pele, depressão, artrite, diabetes e de 
+         mais cânceres, a fim de acertar as ocorrências da coluna que indica presença de doença cardiovascular - 
+         chamado também de coluna <i>target</i>. Após essa predição, é mostrado as porcentagens de acerto das 
+         amostras de treino e de teste.''', unsafe_allow_html=True)
 
 def classification():
     df = pd.read_parquet('data/cvd_cleaned.parquet')
@@ -41,7 +44,7 @@ def classification():
     if len(selected_features) < 2:
         st.error("Selecione pelo menos duas características.")
     else:
-        algorithm = st.selectbox("Escolha o Algoritmo", ["Random Forest", "Árvore de Decisão"])
+        algorithm = st.selectbox("Escolha o Algoritmo", ["Random Forest", "Árvore de Decisão", "kNN", "SVM"])
 
         features_and_target = selected_features + ['Heart_Disease']
         df_selected = df[features_and_target]
@@ -60,8 +63,12 @@ def classification():
 
         if algorithm == "Random Forest":
             model = RandomForestClassifier(random_state=42)
-        else:
+        elif algorithm == "Árvore de Decisão":
             model = DecisionTreeClassifier(random_state=42)
+        elif algorithm == "kNN":
+            model = KNeighborsClassifier()
+        elif algorithm == "SVM":
+            model = SVC(kernel='linear', random_state=42)
 
         model.fit(X_train, y_train)
 
@@ -93,7 +100,7 @@ def classification():
 
         st.write('----')
 
-        if algorithm == "Random Forest":
+        if algorithm == "Random Forest" or algorithm == "Árvore de Decisão":
             model.fit(X_train, y_train)
             feature_importances = model.feature_importances_
             feature_importance_df = pd.DataFrame({
@@ -105,27 +112,31 @@ def classification():
 
             st.write("**<i>Feature importance</i>**", unsafe_allow_html=True)
             st.write('''Essa métrica quantifica a influência de cada característica na capacidade 
-                     preditiva do modelo, permitindo identificar quais variáveis têm maior impacto na 
-                     predição do resultado. <i>Feature importance</i> é crucial para compreender o papel de 
-                     cada variável no desempenho do modelo e pode auxiliar na seleção e otimização das 
-                     características para melhorar a eficácia da predição.''', unsafe_allow_html=True)
+                        preditiva do modelo, permitindo identificar quais variáveis têm maior impacto na 
+                        predição do resultado. <i>Feature importance</i> é crucial para compreender o papel de 
+                        cada variável no desempenho do modelo e pode auxiliar na seleção e otimização das 
+                        características para melhorar a eficácia da predição.''', unsafe_allow_html=True)
             st.table(feature_importance_df)
-        else:
+        elif algorithm == "SVM":
             model.fit(X_train, y_train)
-            feature_importances = model.feature_importances_
-            feature_importance_df = pd.DataFrame({
+            coefficients = model.coef_[0]
+            feature_importance = pd.DataFrame({
                 'Característica': X.columns,
-                'Importância': feature_importances
+                'Importância': coefficients
             })
 
-            feature_importance_df = feature_importance_df.sort_values(by='Importância', ascending=False)
+            feature_importance = feature_importance.sort_values(by='Importância', ascending=False)
 
             st.write("**<i>Feature importance</i>**", unsafe_allow_html=True)
             st.write('''Essa métrica quantifica a influência de cada característica na capacidade 
-                     preditiva do modelo, permitindo identificar quais variáveis têm maior impacto na 
-                     predição do resultado. <i>Feature importance</i> é crucial para compreender o papel de 
-                     cada variável no desempenho do modelo e pode auxiliar na seleção e otimização das 
-                     características para melhorar a eficácia da predição.''', unsafe_allow_html=True)
-            st.table(feature_importance_df)
+                        preditiva do modelo, permitindo identificar quais variáveis têm maior impacto na 
+                        predição do resultado. <i>Feature importance</i> é crucial para compreender o papel de 
+                        cada variável no desempenho do modelo e pode auxiliar na seleção e otimização das 
+                        características para melhorar a eficácia da predição.''', unsafe_allow_html=True)
+            st.table(feature_importance)
+        elif algorithm == "kNN":
+            st.write('''Não foi possível gerar a <i>feature importance</i> do algoritmo kNN''', unsafe_allow_html=True)
+
+        
 
 classification()
